b58da63859b3d2e7406d8746f62e1ce5ab870fc1
poplawski_r <poplawski@tecna.pl>
2016-06-20 08:37:05 +0200

    Init repo

---
+++ LeyLines/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LeyLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btGo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(4, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(535, 505);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(12, 12);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 1;
+            this.btGo.Text = "Go";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 586);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ LeyLines/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LeyLines
+{
+    public partial class Form1 : Form
+    {
+        Point[] pts;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int l = 0;
+            if (pts != null)
+            {
+                //for (int i = 0; i < pts.Length-1; i++)
+                //{
+                //    for (int j = i + 1; j < pts.Length; j++)
+                //    {
+                //        e.Graphics.DrawLine(Pens.HotPink, pts[i], pts[j]);
+                //    }
+                //}
+                foreach (Point pt in pts)
+                {
+                    e.Graphics.DrawEllipse(Pens.Blue, pt.X - 3, pt.Y - 3, 5, 5);
+                }
+                for (int i0 = 0; i0 < pts.Length - 2; i0++)
+                {
+                    for (int i1 = i0 + 1; i1 < pts.Length - 1; i1++)
+                    {
+                        for (int i2 = i1 + 1; i2 < pts.Length - 2; i2++)
+                        {
+                            if ((pts[i1].Y - pts[i0].Y) * (pts[i2].X - pts[i1].X) ==
+                                (pts[i2].Y - pts[i1].Y) * (pts[i1].X - pts[i0].X))
+                            {
+                               //e.Graphics.DrawLine(new Pen(Color.FromArgb(l*1228 | 0x7f000000), 3), pts[i0], pts[i1]);
+                               e.Graphics.DrawLine(new Pen(Color.Pink, 3), pts[i0], pts[i1]);
+                               e.Graphics.DrawLine(Pens.Green, pts[i1], pts[i2]);
+                               l++;
+                            }
+                        }
+                    }
+                }
+                textBox2.Text = l.ToString();
+            }
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(10) + 200;
+            textBox1.Text = n.ToString();
+            pts = new Point[n];
+            for (int i = 0; i < n; i++)
+            {
+                pts[i] = new Point(rnd.Next(panel1.Width), rnd.Next(panel1.Height));
+            }
+            Refresh();
+        }
+    }
+}
